befd04db33b253b38a1a875cc6e1bc61a2f1bb30
NeoloadT <c.reddy@tricentis.com>
2022-12-06 12:56:28 +0530

    Test_demo

---
+++ team/vus/@browser@user_@create_report/%resources%/scripts/jsAction_3d8e5d2e-4ef8-4be6-b8c3-6e051908d0d2.js
@@ -0,0 +1,5 @@
+﻿var OrderID = context.variableManager.getValue("nl_token");
+var writer = new java.io.FileWriter("C:\\Users\\chire\\OneDrive - TRICENTIS\\Desktop\\API_Testing\\OrderID.txt",true);
+writer.write(OrderID);
+writer.write("\r\n");
+writer.close();
